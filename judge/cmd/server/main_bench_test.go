8c666e2fb30bbfd3ccaf6ba20fec6a84a33ae554
liyu-mi829 <17830401975@163.com>
2026-04-01 08:34:59 +0800

    编译运行服务完成

--- judge/cmd/server/main_bench_test.go
+++ judge/cmd/server/main_bench_test.go
@@ -28,8 +28,12 @@ func init() {
 // ==============================================
 // 场景 1：极短代码（最简单打印）
 // ==============================================
-func BenchmarkExecute_ShortCode(b *testing.B) {
-	code := `package main;func main(){println(1)}`
+func BenchmarkExecute_ShortCode_Go(b *testing.B) {
+	code := `package main
+import "fmt"
+func main() {
+	fmt.Print(1)
+}`
 	req := &execute.ExecuteRequest{
 		CodeType:  1,
 		Code:      code,
@@ -51,18 +55,15 @@ func BenchmarkExecute_ShortCode(b *testing.B) {
 // ==============================================
 // 场景 2：中等长度代码（正常逻辑）
 // ==============================================
-func BenchmarkExecute_MidCode(b *testing.B) {
-	code := `
-package main
+func BenchmarkExecute_MidCode_Go(b *testing.B) {
+	code := `package main
 import "fmt"
-func main() {
-	a := 0
-	for i := 0; i < 1000; i++ {
-		a += i
-	}
-	fmt.Println(a)
-}
-`
+func test(a,b int)int{return a+b}
+func main(){
+	s:=0
+	for i:=0;i<100;i++{s+=test(i,i*2)}
+	fmt.Println("result:",s)
+}`
 	req := &execute.ExecuteRequest{
 		CodeType:  1,
 		Code:      code,
@@ -82,23 +83,25 @@ func main() {
 // ==============================================
 // 场景 3：长代码（大量逻辑、多行）
 // ==============================================
-func BenchmarkExecute_LongCode(b *testing.B) {
-	code := `
-package main
-import "fmt"
-func test1() int { return 1 }
-func test2() int { return test1() + 2 }
-func test3() int { return test2() + 3 }
-func test4() int { return test3() + 4 }
-func test5() int { return test4() + 5 }
-func test6() int { return test5() + 6 }
-func test7() int { return test6() + 7 }
-func test8() int { return test7() + 8 }
-func test9() int { return test8() + 9 }
-func main() {
-	fmt.Println(test9())
-}
-`
+func BenchmarkExecute_LongCode_Go(b *testing.B) {
+	code := `package main
+	import "fmt"
+	type Data struct{ID int;Value string}
+	func NewData(id int,v string)*Data{return &Data{id,v}}
+	func (d *Data)Show(){fmt.Println(d.ID,d.Value)}
+	func test1()int{return 123}
+	func test2()string{return "hello"}
+	func main(){
+		list:=make([]*Data,0,100)
+		for i:=0;i<100;i++{
+			list=append(list,NewData(i,test2()))
+		}
+		sum:=0
+		for _,v:=range list{
+			sum+=v.ID;v.Show()
+		}
+		fmt.Println("sum:",sum,test1())
+	}`
 	req := &execute.ExecuteRequest{
 		CodeType:  1,
 		Code:      code,
@@ -118,7 +121,7 @@ func main() {
 // ==============================================
 // 场景 4：递归代码（深度递归，测试性能）
 // ==============================================
-func BenchmarkExecute_RecurseCode(b *testing.B) {
+func BenchmarkExecute_RecurseCode_Go(b *testing.B) {
 	code := `
 package main
 import "fmt"
@@ -149,18 +152,73 @@ func main() {
 // ==============================================
 // 场景 5：计算密集型（压测CPU）
 // ==============================================
-func BenchmarkExecute_CPUHeavyCode(b *testing.B) {
-	code := `
-package main
-func main() {
-	a := 0
-	for i := 0; i < 1000000; i++ {
-		a += i
+func BenchmarkExecute_ShortCode_Cpp(b *testing.B) {
+	code := `#include<iostream>
+using namespace std;
+int main() {
+	cout<<1;
+	return 0;
+}`
+	req := &execute.ExecuteRequest{
+		CodeType:  2,
+		Code:      code,
+		CpuLimit:  3000_000_000,
+		MemoLimit: 64 * 1024,
+	}
+
+	b.ResetTimer()
+	for b.Loop() {
+		_, err := benchClient.ExecuteCode(context.Background(), req)
+		if err != nil {
+			b.Fatal(err)
+		}
 	}
 }
-`
+
+func BenchmarkExecute_MediumCode_Cpp(b *testing.B) {
+	code := `#include <iostream>
+using namespace std;
+int add(int a,int b){return a+b;}
+int main(){
+	int s=0;
+	for(int i=0;i<100;i++)s+=add(i,i*2);
+	cout<<s<<endl;
+	return 0;
+}`
 	req := &execute.ExecuteRequest{
-		CodeType:  1,
+		CodeType:  2,
+		Code:      code,
+		CpuLimit:  3000_000_000,
+		MemoLimit: 64 * 1024,
+	}
+
+	b.ResetTimer()
+	for b.Loop() {
+		_, err := benchClient.ExecuteCode(context.Background(), req)
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func BenchmarkExecute_LongCode_Cpp(b *testing.B) {
+	code := `#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+struct Data{int id;string val;};
+Data newData(int id,string v){return {id,v};}
+void show(Data d){cout<<d.id<<" "<<d.val<<endl;}
+int main(){
+	vector<Data> vec;
+	for(int i=0;i<100;i++) vec.push_back(newData(i,"test"));
+	int sum=0;
+	for(auto& d:vec){sum+=d.id;show(d);}
+	cout<<"sum="<<sum<<endl;
+	return 0;
+}`
+	req := &execute.ExecuteRequest{
+		CodeType:  2,
 		Code:      code,
 		CpuLimit:  3000_000_000,
 		MemoLimit: 64 * 1024,
@@ -176,19 +234,45 @@ func main() {
 }
 
 ///////////////////////////////////////////////////////
-// 																70~120ms之间
-// BenchmarkExecute_ShortCode-4                  16          73971987 ns/op           15689 B/op        137 allocs/op
-// BenchmarkExecute_ShortCode-4                  16          76133474 ns/op            5166 B/op         87 allocs/op
-// BenchmarkExecute_ShortCode-4                  16          73560451 ns/op            5138 B/op         87 allocs/op
-// BenchmarkExecute_MidCode-4                     9         115705742 ns/op            5273 B/op         88 allocs/op
-// BenchmarkExecute_MidCode-4                    10         108931336 ns/op            5266 B/op         88 allocs/op
-// BenchmarkExecute_MidCode-4                    10         108366896 ns/op            5288 B/op         88 allocs/op
-// BenchmarkExecute_LongCode-4                   10         109829918 ns/op            5548 B/op         87 allocs/op
-// BenchmarkExecute_LongCode-4                    9         112430149 ns/op            5586 B/op         88 allocs/op
-// BenchmarkExecute_LongCode-4                    9         113785745 ns/op            5566 B/op         87 allocs/op
-// BenchmarkExecute_RecurseCode-4                 9         122512453 ns/op            5280 B/op         87 allocs/op
-// BenchmarkExecute_RecurseCode-4                 9         115786747 ns/op            5275 B/op         87 allocs/op
-// BenchmarkExecute_RecurseCode-4                 9         111151998 ns/op            5291 B/op         88 allocs/op
-// BenchmarkExecute_CPUHeavyCode-4               15          76960555 ns/op            5168 B/op         86 allocs/op
-// BenchmarkExecute_CPUHeavyCode-4               14          73921039 ns/op            5185 B/op         86 allocs/op
-// BenchmarkExecute_CPUHeavyCode-4               15          72635903 ns/op            5166 B/op         86 allocs/op
+// 															100~300ms之间
+// BenchmarkExecute_ShortCode_Go-4               10         109075201 ns/op
+// BenchmarkExecute_ShortCode_Go-4               10         109672968 ns/op
+// BenchmarkExecute_ShortCode_Go-4               10         109786091 ns/op
+// BenchmarkExecute_ShortCode_Go-4               10         110348252 ns/op
+// BenchmarkExecute_ShortCode_Go-4               10         110640906 ns/op
+
+// BenchmarkExecute_MidCode_Go-4                 10         115969695 ns/op
+// BenchmarkExecute_MidCode_Go-4                  9         115524553 ns/op
+// BenchmarkExecute_MidCode_Go-4                  9         114395183 ns/op
+// BenchmarkExecute_MidCode_Go-4                  9         116039551 ns/op
+// BenchmarkExecute_MidCode_Go-4                 10         108689196 ns/op
+
+// BenchmarkExecute_LongCode_Go-4                10         107512182 ns/op
+// BenchmarkExecute_LongCode_Go-4                10         109466593 ns/op
+// BenchmarkExecute_LongCode_Go-4                10         111880932 ns/op
+// BenchmarkExecute_LongCode_Go-4                10         110081228 ns/op
+// BenchmarkExecute_LongCode_Go-4                10         117567914 ns/op
+
+// BenchmarkExecute_RecurseCode_Go-4             10         111404591 ns/op
+// BenchmarkExecute_RecurseCode_Go-4              9         113388912 ns/op
+// BenchmarkExecute_RecurseCode_Go-4             10         109063515 ns/op
+// BenchmarkExecute_RecurseCode_Go-4             10         107941414 ns/op
+// BenchmarkExecute_RecurseCode_Go-4              9         111985014 ns/op
+
+// BenchmarkExecute_ShortCode_Cpp-4               4         279471396 ns/op
+// BenchmarkExecute_ShortCode_Cpp-4               4         267665606 ns/op
+// BenchmarkExecute_ShortCode_Cpp-4               4         291253578 ns/op
+// BenchmarkExecute_ShortCode_Cpp-4               4         266253802 ns/op
+// BenchmarkExecute_ShortCode_Cpp-4               4         262743927 ns/op
+
+// BenchmarkExecute_MediumCode_Cpp-4              4         269005291 ns/op
+// BenchmarkExecute_MediumCode_Cpp-4              4         265793952 ns/op
+// BenchmarkExecute_MediumCode_Cpp-4              4         281753316 ns/op
+// BenchmarkExecute_MediumCode_Cpp-4              4         270195343 ns/op
+// BenchmarkExecute_MediumCode_Cpp-4              4         272684110 ns/op
+
+// BenchmarkExecute_LongCode_Cpp-4                3         334136179 ns/op
+// BenchmarkExecute_LongCode_Cpp-4                4         332150854 ns/op
+// BenchmarkExecute_LongCode_Cpp-4                3         337274232 ns/op
+// BenchmarkExecute_LongCode_Cpp-4                3         352949292 ns/op
+// BenchmarkExecute_LongCode_Cpp-4                3         355002692 ns/op
--- judge/go.mod
+++ judge/go.mod
@@ -3,7 +3,10 @@ module online-oj/judge
 go 1.25.5
 
 require (
+	github.com/stretchr/testify v1.8.1
 	go.uber.org/zap v1.27.1
+	google.golang.org/grpc v1.79.3
+	online-oj/api v0.0.0
 	online-oj/pkg v0.0.0
 )
 
@@ -13,6 +16,14 @@ replace (
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
+	golang.org/x/net v0.48.0 // indirect
+	golang.org/x/sys v0.39.0 // indirect
+	golang.org/x/text v0.32.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20251202230838-ff82c1b0f217 // indirect
+	google.golang.org/protobuf v1.36.10 // indirect
 	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
