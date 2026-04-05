2eb55c59c676f4105d01a7435f9f0d99e67dd017
liyu-mi829 <17830401975@163.com>
2026-04-05 11:50:22 +0800

    修改数据库表结构

--- gateway/internal/model/entity/languagetemplate.go
+++ gateway/internal/model/entity/languagetemplate.go
@@ -4,14 +4,15 @@ import "time"
 
 // ProblemLanguageTemplate 对应 problem_language_template 表
 type ProblemLanguageTemplate struct {
-	ID           uint64    `gorm:"column:id;primaryKey;autoIncrement" json:"id"`
-	ProblemID    uint64    `gorm:"column:problem_id;index:idx_problem_language,unique" json:"problem_id"`
-	Language     string    `gorm:"column:language;index:idx_problem_language,unique" json:"language"` // go/cpp
-	TemplateCode string    `gorm:"column:template_code" json:"template_code"`                         // 预设代码
-	TestCode     string    `gorm:"column:test_code" json:"test_code"`                                 // 测试代码
-	Enabled      bool      `gorm:"column:enabled" json:"enabled"`                                     // 是否启用
-	CreateTime   time.Time `gorm:"column:create_time" json:"create_time"`
-	UpdateTime   time.Time `gorm:"column:update_time" json:"update_time"`
+	ID           uint64    `gorm:"column:id;primaryKey;autoIncrement;comment:自增主键" json:"id"`
+	ProblemID    uint64    `gorm:"column:problem_id;not null;index:idx_problem_language,unique;comment:题目ID" json:"problem_id"`
+	Language     string    `gorm:"column:language;type:varchar(32);not null;index:idx_problem_language,unique;comment:编程语言，如 go/cpp" json:"language"`
+	PrependCode  string    `gorm:"column:prepend_code;type:mediumtext;not null;comment:隐藏的头部代码，如 Go package/import 或 C++ include" json:"prepend_code"`
+	TemplateCode string    `gorm:"column:template_code;type:mediumtext;not null;comment:用户可见的预设代码" json:"template_code"`
+	TestCode     string    `gorm:"column:test_code;type:mediumtext;not null;comment:评测入口代码" json:"test_code"`
+	Enabled      bool      `gorm:"column:enabled;type:tinyint(1);not null;default:1;comment:是否启用" json:"enabled"`
+	CreateTime   time.Time `gorm:"column:create_time;autoCreateTime;comment:创建时间" json:"create_time"`
+	UpdateTime   time.Time `gorm:"column:update_time;autoUpdateTime;comment:更新时间" json:"update_time"`
 }
 
 func (ProblemLanguageTemplate) TableName() string {
